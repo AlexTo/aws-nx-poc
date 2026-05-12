e628116149cfa01a02f200bf720254040b312d1a
Alex To <tonhudung@gmail.com>
2026-05-12 16:40:39 +1000

    update gen_connections.sh

--- gen_connections.sh
+++ gen_connections.sh
@@ -7,5 +7,8 @@ pnpm exec nx generate @aws/nx-plugin:connection --sourceProject=@aws-nx-poc/smit
 pnpm exec nx generate @aws/nx-plugin:connection --sourceProject=@aws-nx-poc/agents --targetProject=@aws-nx-poc/my-sql-db --sourceComponent=http-agent --no-interactive
 pnpm exec nx generate @aws/nx-plugin:connection --sourceProject=@aws-nx-poc/agents --targetProject=@aws-nx-poc/postgres-db --sourceComponent=http-agent --no-interactive
 
-pnpm exec nx generate @aws/nx-plugin:connection --sourceProject=@aws-nx-poc/agents --targetProject=@aws-nx-poc/my-sql-db --sourceComponent=a2a-agent --no-interactive 
-pnpm exec nx generate @aws/nx-plugin:connection --sourceProject=@aws-nx-poc/agents --targetProject=@aws-nx-poc/postgres-db --sourceComponent=a2a-agent --no-interactive 
+pnpm exec nx generate @aws/nx-plugin:connection --sourceProject=@aws-nx-poc/agents --targetProject=@aws-nx-poc/my-sql-db --sourceComponent=a2a-agent --no-interactive
+pnpm exec nx generate @aws/nx-plugin:connection --sourceProject=@aws-nx-poc/agents --targetProject=@aws-nx-poc/postgres-db --sourceComponent=a2a-agent --no-interactive
+
+pnpm exec nx generate @aws/nx-plugin:connection --sourceProject=@aws-nx-poc/agents --targetProject=@aws-nx-poc/my-sql-db --sourceComponent=mcp-server --no-interactive
+pnpm exec nx generate @aws/nx-plugin:connection --sourceProject=@aws-nx-poc/agents --targetProject=@aws-nx-poc/postgres-db --sourceComponent=mcp-server --no-interactive
