7a25032a012201158aeb1901d45b9948c1b575ca
Alex To <tonhudung@gmail.com>
2026-05-16 23:33:55 +1000

    add gen script

--- gen_base.sh
+++ gen_base.sh
@@ -1,1 +1,13 @@
-pnpm exec nx generate @aws/nx-plugin:ts#dynamodb --name=MyTable --no-interactive
+pnpm exec nx generate @aws/nx-plugin:ts#dynamodb --name=TableOne --directory=packages/dynamo-tables --no-interactive 
+pnpm exec nx generate @aws/nx-plugin:ts#dynamodb --name=TableTwo --directory=packages/dynamo-tables --no-interactive
+
+pnpm exec nx generate @aws/nx-plugin:ts#trpc-api --name=trpc --auth=None --no-interactive 
+pnpm exec nx generate @aws/nx-plugin:ts#smithy-api --name=smithy --auth=None --no-interactive
+
+pnpm exec nx generate @aws/nx-plugin:ts#project --name=agents --no-interactive
+
+pnpm exec nx generate @aws/nx-plugin:ts#strands-agent --project=@aws-nx-poc/agents --name=HttpAgent --no-interactive
+pnpm exec nx generate @aws/nx-plugin:ts#strands-agent --project=@aws-nx-poc/agents --protocol=A2A --name=A2AAgent --no-interactive
+pnpm exec nx generate @aws/nx-plugin:ts#mcp-server --project=@aws-nx-poc/agents --no-interactive
+
+pnpm exec nx generate @aws/nx-plugin:ts#react-website --name=website --uxProvider=Shadcn --no-interactive 
